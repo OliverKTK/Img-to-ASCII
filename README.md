6a44831e46fef1365a21ac4eb34b692c0bc9acd5
Oliver Kuramae <oliverkuramae@usp.br>
2024-07-03 13:37:08 -0300

    Update README and reafctor some variables

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Img-to-ASCII
-Converts an image into a ASCII art maintaining the image ratio and colors 
+Written in Python using PILLOW library
+## Converts an image into a ASCII art
+1. Clone the repository:
+
+```
+$ git clone git@github.com:OliverKTK/Img-to-ASCII.git
+```
+
+2. Change variables:
+	Change `path` and `fontPath`. Can also change spacing between letters and the scale in relation to the original image 
+
+``` 
+charWidth = 11
+charHeight = 13
+scale = 0.1 # where 1 is a 1:1 scale
+path = 'image.jpg'
+fontPath = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
+nameOutput = 'output'
+```
+
+3. Run the code
+	An image output will be created in the code directory
+
 ![alt text](https://github.com/OliverKTK/Img-to-ASCII/blob/main/monalisa_ascii.png?raw=true)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
 import math
 
+# Variables used for the output image
+charWidth = 11
+charHeight = 13
+scale = 0.1
+ImgPath = 'monalisa.jpg'
+fontPath = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
+nameOutput = 'output'
+
 
 # Characters used for the ascii art
 # chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`. "[::-1]
@@ -11,10 +19,6 @@ chars = "$B8W#ohbpwZ0LJYzvnrf/|){[?_~>!I:^. "[::-1]
 charArray = list(chars)
 charLen = len(charArray)
 interval = charLen/256
-
-# The Height and Width of eacch character
-charWidth = 11
-charHeight = 13
 
 # Gets the char for the value
 def getChar(value):
@@ -29,9 +33,9 @@ def imageResize(image, delta, charWidth, charHeight):
 
 
 # Opens image/ gets font used and resizes it
-im = Image.open("duck.jpg").convert("RGB")
-font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 15)
-width, height = imageResize(im, 0.1, charWidth, charHeight)
+im = Image.open(ImgPath).convert("RGB")
+font = ImageFont.truetype(fontPath, 15)
+width, height = imageResize(im, scale, charWidth, charHeight)
 im = im.resize((width, height))
 pix = im.load()
 
@@ -50,5 +54,5 @@ for i in range(width):
         d.text((i*charWidth, j*charHeight), getChar(value), font= font, fill = (r, g, b))
 
 # Saves the result
-outputImg.save('output.png')
+outputImg.save(nameOutput+'.png')
 
